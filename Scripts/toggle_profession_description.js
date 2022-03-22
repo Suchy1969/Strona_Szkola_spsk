296a98432ace303a99a87308a4c5f1ae2c69ebb3
krzysztof-cieslik <mrkrzysiex@gmail.com>
2022-03-22 18:21:12 +0100

    Zmieniono działanie rozwijania opisów zawodów

---
+++ Scripts/toggle_profession_description.js
@@ -0,0 +1,16 @@
+﻿function toggle_profession_description(profession) {
+    let allProfessions = document.getElementsByClassName("profession");
+
+    Array.from(allProfessions).forEach(el => {
+        console.log(el.id.split(' '));
+        if (el.id.split(' ')[0] === profession) {
+            if (el.style.display === "block") {
+                el.style.display = "none";
+            } else {
+                el.style.display = "block";
+            }
+        } else {
+            el.style.display = "none";
+        }
+    })
+}
